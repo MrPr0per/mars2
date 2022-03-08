12ad4e2e7d3aacddeae4a4d49ba3fed6bfe01c16
dev <eloper>
2022-03-08 15:36:46 +0500

    Тренировки в полёте /training/<prof>

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -8,6 +8,24 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 @app.route('/index/<title>')
 def index(title):
     return render_template('index.html', title=title)
+
+
+@app.route('/training/<prof>')
+def training(prof):
+    if 'инженер' in prof or 'строитель' in prof:
+        text = 'Инженерные тренажеры'
+        url = url_for('static', filename='img/1.png')
+        alt = 'местоположение инженерных тренажеров'
+    elif 'ученый' in prof:
+        text = 'Научные тренажеры'
+        url = url_for('static', filename='img/2.png')
+        alt = 'местоположение научных тренажеров '
+    else:
+        text = 'такой профессии нет в списке!'
+        url = url_for('static', filename='img/exit.png')
+        alt = '???????????????????????????'
+
+    return render_template('index.html', prof=prof, text=text, url=url, alt=alt)
 
 
 if __name__ == '__main__':
